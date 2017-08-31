e0e47ac590e49a2f12347762cc1a2ffe4648bc51
Hugo Estrada <Hugo.Estrada@afge.org>
2017-08-31 16:44:15 -0400

    added prime bonus to the calculations for the character

--- explore.exs
+++ explore.exs
@@ -2,7 +2,9 @@ defmodule Character do
   def roll_character do
     roll = roll_attributes()
     class = roll |> select_class  
-    %{attributes: roll, class: class}
+    prime_requisit_bonus = calculate_prime_requisit_bonus(class, roll)
+
+    %{attributes: roll, class: class, prime_bonus: prime_requisit_bonus}
   end
 
   defp roll_attributes do
@@ -34,6 +36,33 @@ defmodule Character do
       value > max_v -> max_value t, {attribute, value}
       true    -> max_value t, {max_a, max_v}
     end
+  end
+
+  defp calculate_prime_requisit_bonus(:fighter, roll) do
+    int = Keyword.get roll, :intelligence      
+    wis = Keyword.get roll, :wisdom      
+
+    div(max((int - 9), 0), 2) + div(max((wis - 9),0), 3) 
+  end
+
+  defp calculate_prime_requisit_bonus(:wizard, roll) do
+    wis = Keyword.get roll, :wisdom      
+
+    div(max((wis - 9),0), 2)
+  end
+
+  defp calculate_prime_requisit_bonus(:cleric, roll) do
+    str = Keyword.get roll, :strength      
+    int = Keyword.get roll, :intelligence      
+
+    div(max((int - 9), 0), 2) + div(max((str - 9), 0) , 3)
+  end
+
+  defp calculate_prime_requisit_bonus(:thief, roll) do
+    int = Keyword.get roll, :intelligence      
+    wis = Keyword.get roll, :wisdom      
+
+    div(max((int - 9),0), 2) + max((wis - 9), 0)
   end
 
 end
