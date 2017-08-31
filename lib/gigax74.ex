c3cb9c7ec3ff84f95ee15988b4c3662320048146
Hugo Estrada <Hugo.Estrada@afge.org>
2017-08-31 15:02:23 -0400

    created elixir project

---
+++ lib/gigax74.ex
@@ -0,0 +1,18 @@
+defmodule Gigax74 do
+  @moduledoc """
+  Documentation for Gigax74.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gigax74.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gigax74_test.exs
@@ -0,0 +1,8 @@
+defmodule Gigax74Test do
+  use ExUnit.Case
+  doctest Gigax74
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
